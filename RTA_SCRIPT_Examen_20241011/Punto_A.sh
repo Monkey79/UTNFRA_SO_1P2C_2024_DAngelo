69925d8a5f16a4bb892899b142e1ccd52b5541dc
Monkey79 <marianomartin79@gmail.com>
2024-10-24 23:49:14 +0000

    fix punto_a

--- RTA_SCRIPT_Examen_20241011/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241011/Punto_A.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-sudo mkdir -p /Examenes-UTN/{alumno_{1/parcial_{1,2,3},2/parcial_{1,2,3},3/parcial_{1,2,3}},profesores}
+#sudo mkdir -p /Examenes-UTN/{alumno_{1/parcial_{1,2,3},2/parcial_{1,2,3},3/parcial_{1,2,3}},profesores}
+sudo mkdir -p /Examenes-UTN/{alumno_{1..3}/parcial{1..3},profesores}
 echo "-------------------------------puanto_a:status--------------------------------------------"
 sudo tree /Examenes-UTN/
 sudo ls -ld /Examenes-UTN/
